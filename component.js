ad4e3cd9449dff384423c0df1ef650c3b535db6b
g7tro4w <g7tro4w@gmail.com>
2022-03-13 20:38:38 +0300

    Small stuff in component factory

--- component.js
+++ component.js
@@ -1,6 +1,31 @@
 (function () {
+    const parseName = (name) => {
+        return name
+    }
+
     const factory = function (name, html, injectToElement) {
         injectToElement.innerHtml = html
+
+        class ComponentClass extends HTMLElement {
+            render () {
+                this.isRendered = true;
+                this.innerHTML = html
+            }
+
+            connectedCallback () {
+                if (!this.isRendered) {
+                    this.render();
+                }
+            }
+
+            // static get observedAttributes() {
+            //     return [];
+            // }
+            // attributeChangedCallback(name, oldValue, newValue) {
+            // }
+        }
+
+        customElements.define(parseName(name), ComponentClass);
     }
     window.__componentCreator__ = factory
 })()
